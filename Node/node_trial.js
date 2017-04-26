ccd2a688e762deb9e0e7b06d1b7f637fe7700a7e
yaknad <yaknad@gmail.com>
2017-04-26 23:03:11 +0300

    npm lesson 12 + node references

--- Node/node_trial.js
+++ Node/node_trial.js
@@ -3,3 +3,9 @@
 var _ = require('lodash');
 var arr = _.without([1, 2, 3], 2);
 console.log(arr);
+
+// export a module
+
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+}
