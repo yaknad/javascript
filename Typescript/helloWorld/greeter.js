c85e5cb57b6eb9b4558422eff8be2ff8ee49fe3e
yn804c <yn804c@intl.att.com>
2018-11-21 18:22:33 +0200

    [Typescript] beginning - a gulp tutorial

---
+++ Typescript/helloWorld/greeter.js
@@ -0,0 +1,17 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User"); // { firstName: "Jane", lastName: "User"}; //[0,1,2];//"Jane User";
+document.body.innerHTML = greeter(user);
+
+
+
