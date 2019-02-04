a7046c184c9226e7f057e49099624b6649347046
yn804c <yn804c@intl.att.com>
2019-02-04 18:32:30 +0200

    more basic types

--- Typescript/gulp/proj/src/main.ts
+++ Typescript/gulp/proj/src/main.ts
@@ -79,8 +79,22 @@ My bckground color is ${background_color2}`;
 let numbersArray: number[] = [1,2,3];
 let numberArray2: number[] = [1, 1.5, 0xf00d];
 
-
 showString("sentence", sentence);
 showArray("array", numberArray2);
 
+let tupple1: [string, number] = ["I'm a string", 1];
+// let tupple2: [number, string] = ["1", 1]; // compile error
+let str : string = tupple1[0];
+// let num : number = tupple1[0]; // compile error
 
+
+
+enum Colors { Red, Yellow, Blue }; // values numbering is default (0,1,2)
+enum Colors2 { Red = 1, Yellow, Blue }; // values numbering will start with 1, and continue incrementally
+enum Colors3 { Red = 1, Yellow = 2, Blue = 4 };  // manually setting all values  
+
+let color_1 : Colors = Colors.Red; 
+let color_2: string = Colors[0]; // Red --> Note the type is string, not Color! (see color_3 later)
+color_2 = Colors2[1]; // Red
+color_2 = Colors3[4]; // Blue
+let color_3 : Colors = Colors["Red"]; // Note the type is Color (not like in color_2 above)
