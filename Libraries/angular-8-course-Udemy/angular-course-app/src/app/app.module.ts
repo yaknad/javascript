866d5e293b022354ffa58a5de9cbabae399123a0
yaknad <yaknad@gmail.com>
2020-04-02 00:09:32 +0300

    stopped at lecture 19

--- Libraries/angular-8-course-Udemy/angular-course-app/src/app/app.module.ts
+++ Libraries/angular-8-course-Udemy/angular-course-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // all the components we use (unless auto-scanning)
+    AppComponent,
+    ServerComponent
   ],
-  imports: [
+  imports: [ // add other modules to our module
     BrowserModule,
-    // FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // the components angular should know when bootstrapping the app.
 })
 export class AppModule { }
